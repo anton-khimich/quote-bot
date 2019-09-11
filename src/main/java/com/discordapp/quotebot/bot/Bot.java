3f7c5d69a5a8cd5392bbedd039e93d5d03c110ca
Anton <anton.khimich@gmail.com>
2019-09-10 20:40:11 -0400

    Work on bot commands

--- src/main/java/com/discordapp/quotebot/bot/Bot.java
+++ src/main/java/com/discordapp/quotebot/bot/Bot.java
@@ -1,13 +1,19 @@
 package com.discordapp.quotebot.bot;
 
+import com.discordapp.quotebot.entity.Quote;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.LoginException;
+import java.time.LocalDate;
+import java.util.MissingFormatArgumentException;
 
 @Service
 public class Bot {
+    private static Quote dailyQuote;
+    private static LocalDate dailyQuoteDate;
 
     public Bot() {
         try {
@@ -19,6 +25,36 @@ public class Bot {
         } catch (LoginException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    static Quote getDailyQuote() {
+        LocalDate dateNow = LocalDate.now();
+        if (dailyQuote == null) {
+            //pick a random quote and note the date
+        } else if (dateNow.isAfter(dailyQuoteDate)) {
+            //pick a new quote and note the date
+        }
+        return dailyQuote;
+    }
+
+    static Quote getSpecificQuote(String name) {
+        return new Quote();
+    }
+
+    static void saveQuote(JSONObject obj) {
+        String quoteName = obj.getString("name");
+        String quoteContent = obj.getString("content");
+        String quoteAuthor = obj.getString("author");
+        String quoteContributor = obj.getString("contributor");
+        if (quoteName != null && quoteContent != null && quoteAuthor != null && quoteContributor != null) {
+            // add the quote to the repository
+        } else {
+            throw new IllegalArgumentException("Incorrect quote format.");
+        }
+    }
+
+    static void deleteQuote(String name) {
+
     }
 
 }
--- src/main/java/com/discordapp/quotebot/bot/MyBotListener.java
+++ src/main/java/com/discordapp/quotebot/bot/MyBotListener.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.json.JSONObject;
 
 public class MyBotListener extends ListenerAdapter {
 
@@ -18,25 +19,36 @@ public class MyBotListener extends ListenerAdapter {
         User author = message.getAuthor();
         String content = message.getContentDisplay();
         Message reply;
+
         if (event.isFromType(ChannelType.TEXT)) {
             MessageChannel channel = event.getChannel();
+            // Save the given quote
             if (content.startsWith("!qsave")) {
-                reply = new MessageBuilder().append(author).append("Save quote.").build();
+                content = content.substring(7);
+                content += ",contributor:" + author.toString();
+                JSONObject obj = new JSONObject("{" + content + "}");
+                Bot.saveQuote(obj);
+                reply = new MessageBuilder().append(author).append(" Save quote.").build();
                 channel.sendMessage(reply).queue();
+            // Delete the quote by name
             } else if (content.startsWith("!qdelete")) {
-                reply = new MessageBuilder().append(author).append("Delete quote.").build();
+                content = content.substring(3);
+                Bot.deleteQuote(content);
+                reply = new MessageBuilder().append(author).append(" Delete quote.").build();
                 channel.sendMessage(reply).queue();
+            // Display the list of quotes saved in the discord channel
             } else if (content.startsWith("!qlist")) {
-                reply = new MessageBuilder().append(author).append("Quote list.").build();
+                reply = new MessageBuilder().append(author).append(" Displaying a list of quotes is not yet implemented.").build();
                 channel.sendMessage(reply).queue();
-            } else if (content.startsWith("!q")) {
-                if (content.equals("!q")) {
-                    reply = new MessageBuilder().append(author).append("Daily quote.").build();
-                    channel.sendMessage(reply).queue();
-                } else if (content.matches("!q .+")) {
-                    reply = new MessageBuilder().append(author).append("Specific quote.").build();
-                    channel.sendMessage(reply).queue();
-                }
+            // Display the quote of the day
+            } else if (content.equals("!q")) {
+                reply = new MessageBuilder().append(author).append(Bot.getDailyQuote()).build();
+                channel.sendMessage(reply).queue();
+            // Display a quote by name
+            } else if (content.matches("!q .+")) {
+                content = content.substring(3);
+                reply = new MessageBuilder().append(author).append(" " + Bot.getSpecificQuote(content)).build();
+                channel.sendMessage(reply).queue();
             }
         }
     }
